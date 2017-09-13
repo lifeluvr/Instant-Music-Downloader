68af6b8d35ccce6bc006da1b85e68a8a9d8759b9
Eric Payne <duncecapsoftware@gmail.com>
2017-09-12 21:53:38 -0700

    okay check before updating index

---
+++ eznav.js
@@ -0,0 +1,47 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "260px";
+    document.getElementById("main").style.marginLeft = "260px";
+    document.body.style.backgroundColor = "#fff";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#fff";
+}
+
+function openChat() {
+    document.getElementById("mySidechat").style.width = "480px";
+    document.getElementById("main").style.marginLeft = "480px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+
+function closeChat() {
+    document.getElementById("mySidechat").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#fff";
+}
+		
+function openAbout() {
+    document.getElementById("mySideabout").style.width = "480px";
+    document.getElementById("main").style.marginLeft = "480px";
+    document.body.style.backgroundColor = "#fff";
+}
+
+function closeAbout() {
+    document.getElementById("mySideabout").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#fff";
+}
+function openMore() {
+    document.getElementById("mySidemore").style.width = "480px";
+    document.getElementById("main").style.marginLeft = "480px";
+    document.body.style.backgroundColor = "#fff";
+}
+
+function closeMore() {
+    document.getElementById("mySidemore").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#fff";
+}
